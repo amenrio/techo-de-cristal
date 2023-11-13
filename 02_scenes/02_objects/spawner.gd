f6658f91d3c8aff62ad0cd5bca4bbfc3927354bc
amenrio <amenrio@proton.me>
2023-11-13 23:46:01 +0100

    Enemys Spawn arround the player, Enemies no longer stack on top of each other

--- 02_scenes/02_objects/spawner.gd
+++ 02_scenes/02_objects/spawner.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 @export var spawn_timer: float = 4.0
 @onready var current_level = get_tree().current_scene
+@onready var player = get_parent().get_node("player")
 @onready var spawn_area = $spawn_area
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,6 +14,6 @@ func _ready():
 
 func _on_spawn_timer_timeout():
 	var new_enemy = enemies[0].instantiate()
-	var spawn_position = spawn_area.position + Vector2(randf() * spawn_area.size.x, randf() * spawn_area.size.y)
+	var spawn_position = player.position + Vector2(1000,0).rotated(randf_range(0, 2*PI))
 	new_enemy.position = spawn_position
 	current_level.add_child(new_enemy) # Replace with function body.
