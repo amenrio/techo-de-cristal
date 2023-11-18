6d67b849d1b1f95b3a573605d3769965af5fecfc
Amaya Vicente <amaya.vicente@udc.es>
2023-11-18 20:35:33 +0100

    credits and exit menu buttons work

--- 02_scenes/04_screens/menuScreen.gd
+++ 02_scenes/04_screens/menuScreen.gd
@@ -1,8 +1,16 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _on_b_xogar_pressed():
+	pass
+
+func _on_b_creditos_pressed():
+	get_tree().change_scene_to_file("res://02_scenes/04_screens/credits_screen.tscn")
+
+
+func _on_b_controles_pressed():
 	pass # Replace with function body.
 
 
+func _on_b_sair_pressed():
+	get_tree().quit()
