1c4c8d5435e3586f24d1b6d8bc55825d2dc94953
Amaya Vicente <amaya.vicente@udc.es>
2023-11-18 23:24:32 +0100

    feedback, results aand pause screen created

--- 02_scenes/04_screens/gameScreen.gd
+++ 02_scenes/04_screens/gameScreen.gd
@@ -3,8 +3,8 @@ extends Node2D
 
 
 func _on_b_resultados_pressed():
-	get_tree().change_scene_to_file("res://02_scenes/04_screens/resultados_screen.tscn")
+	get_tree().change_scene_to_file("res://02_scenes/04_screens/feedback_screen.tscn")
 
 
 func _on_b_pausa_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://02_scenes/04_screens/pause_screen.tscn")
---
+++ 02_scenes/04_screens/pause_screen.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+func _on_b_menu_pressed():
+	pass # Replace with function body.
+
+
+func _on_b_volver_pressed():
+	pass # Replace with function body.
