07c6347fc0fda768e512fe234518c2c0f2cfbfde
Amaya Vicente <amaya.vicente@udc.es>
2023-11-18 23:17:52 +0100

    pregame and game buttons work

--- 02_scenes/04_screens/gameScreen.gd
+++ 02_scenes/04_screens/gameScreen.gd
@@ -1,2 +1,10 @@
 extends Node2D
 
+
+
+func _on_b_resultados_pressed():
+	get_tree().change_scene_to_file("res://02_scenes/04_screens/resultados_screen.tscn")
+
+
+func _on_b_pausa_pressed():
+	pass # Replace with function body.
--- 02_scenes/04_screens/pregameScreen.gd
+++ 02_scenes/04_screens/pregameScreen.gd
@@ -1,11 +1,4 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_b_continuar_pressed():
+	get_tree().change_scene_to_file("res://02_scenes/04_screens/game_screen.tscn")
