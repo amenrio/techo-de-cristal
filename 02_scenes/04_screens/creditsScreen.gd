7e97818bc14f562875018e7f82b0e47935d00404
Amaya Vicente <amaya.vicente@udc.es>
2023-11-18 20:40:01 +0100

    button back in credits works

--- 02_scenes/04_screens/creditsScreen.gd
+++ 02_scenes/04_screens/creditsScreen.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
+func _on_b_creditos_atras_pressed():
+	get_tree().change_scene_to_file("res://02_scenes/04_screens/menu_screen.tscn")
