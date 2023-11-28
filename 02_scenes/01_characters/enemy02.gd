6afa53b0cbb160f37b3aad4bbf0f27f54d429bd7
DESKTOP-JTM7MNL\Pablo <rod.lopez.pablo@gmail.com>
2023-11-28 03:05:32 +0100

    Explosion enemy finished. Explosion GPUParticles added. Pathfinding set to use A*

--- 02_scenes/01_characters/enemy02.gd
+++ 02_scenes/01_characters/enemy02.gd
@@ -9,12 +9,13 @@ extends CharacterBody2D
 @onready var health_component = $HealthComponent
 @onready var actRange = $activationRange
 @onready var fuse = $timer
+@onready var nav_agent = $NavigationAgent2D
+@onready var explosion = preload("res://02_scenes/05_others/explosion.tscn")
 
 var max_speed = 300
 var following_player = true
 var damage = 10.0
 var active = false
-var playerRef
 
 var sprite_string = "res://01_assets/01_sprites/enemy_%s.png"
 
@@ -23,15 +24,20 @@ func _ready():
 	$sprite.texture = load(actual_texture)
 	
 func _physics_process(_delta):
+	#if following_player == true:
+	#	velocity = position.direction_to(player_instance.position) * max_speed
+	#else:
+	#	velocity = position.direction_to(player_instance.position)
 	if following_player == true:
-		velocity = position.direction_to(player_instance.position) * max_speed
-	else:
-		velocity = position.direction_to(player_instance.position)
-	move_and_slide()
+		var dir = to_local(nav_agent.get_next_path_position()).normalized()
+		velocity = dir * max_speed
+		move_and_slide()
+
+func makepath():
+	nav_agent.target_position = player_instance.global_position
 
 func _on_hitbox_component_area_entered(area):
 	if area.has_method("damage"):
-		area.damage(damage)
 		following_player = false
 		velocity = Vector2.ZERO
 		await get_tree().create_timer(hit_lag).timeout
@@ -40,7 +46,6 @@ func _on_hitbox_component_area_entered(area):
 
 func _on_activation_range_body_entered(body):
 	if body.name == "player":
-		playerRef = body
 		# following_player = false
 		active = true
 		fuse.start()
@@ -57,4 +62,13 @@ func _on_activation_range_body_exited(body):
 
 func _on_timer_timeout():
 	print("BOOM")
+	var hitbox = player_instance.get_node("HitboxComponent")
+	hitbox.damage(damage * 2)
+	var explosionInstance = explosion.instantiate()
+	explosionInstance.position = self.position
+	get_parent().add_child(explosionInstance)
 	get_tree().queue_delete(self)
+
+
+func _on_nav_timer_timeout():
+	makepath()
