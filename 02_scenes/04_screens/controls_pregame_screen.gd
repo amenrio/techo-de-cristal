7dbdb8145b44e0252e914b2ea828fe12b3ec1a51
gry-ff <amaya.vperez@udc.es>
2023-12-07 18:32:45 +0100

    pre-game screens look and functionality setup

--- 02_scenes/04_screens/controls_pregame_screen.gd
+++ 02_scenes/04_screens/controls_pregame_screen.gd
@@ -1,4 +1,4 @@
 extends Node2D
 
 func _on_b_controles_continuar_pressed():
-	get_tree().change_scene_to_file("res://02_scenes/04_screens/pregame_screen.tscn")
+	get_tree().change_scene_to_file("res://02_scenes/04_screens/recipe_pregame_screen.tscn")
