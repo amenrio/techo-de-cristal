11af84408153181645de817b91ffb8940acad45c
gry-ff <amaya.vperez@udc.es>
2024-01-05 19:57:22 +0100

    buttons recipe nodes created

--- 02_scenes/04_screens/recipe_screen.gd
+++ 02_scenes/04_screens/recipe_screen.gd
@@ -2,3 +2,11 @@ extends Node2D
 
 func _on_b_continuar_pressed():
 	get_tree().change_scene_to_file("res://02_scenes/04_screens/menu_screen.tscn")
+
+
+func _on_b_next_pressed():
+	pass # Replace with function body.
+
+
+func _on_b_previous_pressed():
+	pass # Replace with function body.
