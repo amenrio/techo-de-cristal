227d0182c6b0f89a7137a272c5777bb620ea1657
gry-ff <amaya.vperez@udc.es>
2024-01-06 11:56:52 +0100

    recipe screens arrows work!!! yipeee!

--- 02_scenes/04_screens/recipe_screen.gd
+++ 02_scenes/04_screens/recipe_screen.gd
@@ -1,12 +1,18 @@
 extends Node2D
 
+var pos = 0
+@onready var recipeArray = [get_node("recipeChurros"), get_node("recipeTarta"), get_node("recipeGofres"), get_node("recipeZume")]
+
+func _updateRecipe(direction):
+	recipeArray[pos].hide()
+	pos = (pos + direction) % len(recipeArray)
+	recipeArray[pos].show()
+
 func _on_b_continuar_pressed():
 	get_tree().change_scene_to_file("res://02_scenes/04_screens/menu_screen.tscn")
 
-
 func _on_b_next_pressed():
-	pass # Replace with function body.
-
+	_updateRecipe(+1)
 
 func _on_b_previous_pressed():
-	pass # Replace with function body.
+	_updateRecipe(-1)
